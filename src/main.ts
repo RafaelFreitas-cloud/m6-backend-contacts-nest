9633ef486ac6503528e6793952ac540a42c23471
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-08-04 21:14:12 -0300

    feat/cors configured

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -12,6 +13,9 @@ async function bootstrap() {
       transformOptions: { groups: ['transform'] },
     }),
   );
+  app.enableCors({
+    origin:["http://localhost:5173","https://m6-sprint2-front.vercel.app"]
+  })
   await app.listen(3000);
 }
 bootstrap();
